07d6e575a9865d379be268319c291b5d5845ab88
Soumahoro-Aboubakar <aboubakarkone8958@gmail.com>
2026-04-18 19:11:08 -0400

    travaille sur l'ajout du password

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/a90rchitect_football';
 
+
+
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -29,9 +31,40 @@ app.get('/api/health', (req, res) => {
 mongoose.connect(MONGO_URI)
   .then(() => {
     console.log('✅ MongoDB connecté' , MONGO_URI);
-    app.listen(PORT, () => {
+  /*  app.listen(PORT, () => {
       console.log(`🚀 Serveur démarré sur http://localhost:${PORT}`);
+      require('./seed');
     });
+*/
+
+app.listen(PORT, () => {
+  console.log(`🚀 Serveur démarré sur http://localhost:${PORT}`);
+
+  // Seed uniquement si activé
+  if (process.env.SEED === 'true') {
+    require('./seed');
+  }
+
+  // Auto-ping pour éviter sleep
+  const BACKEND_URL = process.env.BACKEND_URL;
+
+  if (!BACKEND_URL) {
+    console.warn('⚠️ BACKEND_URL non défini, auto-ping désactivé');
+    return;
+  }
+
+  setInterval(async () => {
+    try {
+      const res = await axios.get(`${BACKEND_URL}/api/health`);
+      console.log('🔄 Ping serveur OK:', res.data.status);
+    } catch (err) {
+      console.error('❌ Ping échoué:', err.message);
+    }
+  }, 40000); // 40 secondes
+});
+
+
+
   })
   .catch(err => {
     console.error('❌ Erreur MongoDB:', err.message);
